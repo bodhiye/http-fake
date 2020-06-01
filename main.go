673c9e8acd582e295ff88bc8bf329a46230afde2
yeqiongzhou <yeqiongzhou@qiniu.com>
2020-06-01 11:06:22 +0800

    modify the value of json

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func fake(w http.ResponseWriter, r *http.Request) {
 	// 打印日志
 	fmt.Printf("url=%s\n", req.Data.URI)
 
-	fmt.Fprintf(w, `{"code": 200, "data": [{"code": 200, "msg": "OK", "results": [{"label": "qrcode", "rate": 99.91, "scene": "ad", "suggestion": "block"}], "taskId": "img1gkLHlcQAM44mg2JCKEcz@-1s3ymX"}], "msg": "OK", "requestId": "CF782CC1-CC21-4333-8D5C-57EDD7BBD917"}`)
+	fmt.Fprintf(w, `{"code":200,"data":[{"code":200,"dataId":"3fc2ac30-5ed3-4bb3-a0d5-e3d919858a2f","extras":{},"msg":"OK","results":[{"label":"sexy","rate":99.9,"scene":"porn","suggestion":"pass"}],"taskId":"img7$NtJ82sHRH7gY9QTn7eP5-1sCSXn","url":"http://cdn.yeqiongzhou.top/pulp.jpg"}],"msg":"OK","requestId":"BECA8CF0-14A6-4E0D-AF80-B2ABEEAE23AD"}`)
 }
 
 func main() {
