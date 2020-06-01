809dced0e8a47d5188c92c133524ec600c502792
yeqiongzhou <yeqiongzhou@qiniu.com>
2020-06-01 13:34:51 +0800

    modify the value of json

--- main.go
+++ main.go
@@ -6,13 +6,15 @@ import (
 	"net/http"
 )
 
+type Task struct {
+	DataId string `json:"dataId"` // 数据Id，需要保证在一次请求中所有的Id不重复。
+	Url    string `json:"url"`    // 待检测图像的URL
+}
+
 type fakeReq struct {
-	Data struct {
-		URI string `json:"uri"`
-	} `json:"data"`
-	Params struct {
-		Scenes []string `json:"scenes"`
-	} `json:"params"`
+	BizType string   `json:"bizType"` // 标识业务场景
+	Scenes  []string `json:"scenes"`  // 指定图片检测的应用场景
+	Tasks   []Task   `json:"tasks"`   // 指定检测对象
 }
 
 // 处理application/json类型的POST请求
@@ -24,7 +26,7 @@ func fake(w http.ResponseWriter, r *http.Request) {
 	// 解析参数 存入map
 	decoder.Decode(&req)
 	// 打印日志
-	fmt.Printf("url=%s\n", req.Data.URI)
+	fmt.Printf("url=%s\n", req.Tasks)
 
 	fmt.Fprintf(w, `{"code":200,"data":[{"code":200,"dataId":"3fc2ac30-5ed3-4bb3-a0d5-e3d919858a2f","extras":{},"msg":"OK","results":[{"label":"sexy","rate":99.9,"scene":"porn","suggestion":"pass"}],"taskId":"img7$NtJ82sHRH7gY9QTn7eP5-1sCSXn","url":"http://cdn.yeqiongzhou.top/pulp.jpg"}],"msg":"OK","requestId":"BECA8CF0-14A6-4E0D-AF80-B2ABEEAE23AD"}`)
 }
