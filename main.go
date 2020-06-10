81a9dc4afb8a5f384dc91e4a2cd6689ab16ad268
yeqiongzhou <yeqiongzhou@qiniu.com>
2020-06-10 15:55:03 +0800

    add log

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func fake(w http.ResponseWriter, r *http.Request) {
 	// 打印日志
 	fmt.Printf("url=%s\n", req.Tasks)
 
-	fmt.Fprintf(w, `{"code":200,"data":[{"code":200,"dataId":"3fc2ac30-5ed3-4bb3-a0d5-e3d919858a2f","extras":{},"msg":"OK","results":[{"label":"sexy","rate":99.9,"scene":"porn","suggestion":"pass"}],"taskId":"img7$NtJ82sHRH7gY9QTn7eP5-1sCSXn","url":"http://cdn.yeqiongzhou.top/pulp.jpg"}],"msg":"OK","requestId":"BECA8CF0-14A6-4E0D-AF80-B2ABEEAE23AD"}`)
+	fmt.Fprintf(w, `{"code":200,"data":[{"code":200,"msg":"OK","results":[{"label":"normal","rate":99.91,"scene":"antispam","suggestion":"pass","details":[{"startTime":0,"endTime":26,"text":"快year of the good one so shy that thing need to be your shape of you some of from time to time。","label":"normal"}]}],"taskId":"vc_f_39ILHnyY4Cu7bioU8tJSXD-1sFxLr"}],"msg":"OK","requestId":"105C89C4-4AE8-4896-BD3E-21101F652658"}`)
 }
 
 func main() {
